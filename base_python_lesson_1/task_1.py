60fce6a142d8490208666bc977f72bc55f11afaa
Daniel <Town.kor@gmail.com>
2020-12-16 23:45:32 +0300

    completed task lesson 1

---
+++ base_python_lesson_1/task_1.py
@@ -0,0 +1,10 @@
+a = "Hi"
+b = 42
+print(a, str(b))
+
+name = input('Введите имя: ')
+age = input('Введите возраст: ')
+city = input('Введите город: ')
+
+res = f"{age} летний {name} живет в городе {city}"
+print(res)
